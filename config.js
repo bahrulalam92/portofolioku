77be20eb0bd2afe121d4c97e4e6ff1330bfeea84
wahyuhidayattz <wahyuhidayatsmk2@gmail.com>
2023-04-10 07:00:00 +0700

    Frist Commit

---
+++ config.js
@@ -0,0 +1,185 @@
+let portfolio = [
+    {
+        "image": "waris.png",
+        "title": "WARIS",
+        "year": "2021",
+        "desc": "Wahyu Reporting Information System is a website for reporting sales, stock, and data related to Indomaret store operations. This website is made to faciitate reporting.",
+        "link": "#"
+    },
+    {
+        "image": "php_filemanager.png",
+        "title": "PHP File Manager",
+        "year": "2023",
+        "desc": "Wahyu Reporting Information System is a website for reporting sales, stock, and data related to Indomaret store operations. This website is made to faciitate reporting.",
+        "link": "https://github.com/WahyuHidayattz/php-file-manager"
+    },
+    {
+        "image": "csv_converter.png",
+        "title": "CSV Konverter",
+        "year": "2022",
+        "desc": "Is a tool for converting Google form results into a table that can display images automatically.",
+        "link": "https://github.com/WahyuHidayattz/csv-konverter"
+    },
+    {
+        "image": "crypto.png",
+        "title": "Crypthography Tool",
+        "year": "2022",
+        "desc": "Is a tool to convert plain text into cipher text and vice versa.",
+        "link": "https://github.com/WahyuHidayattz/cryptography-generator"
+    },
+    {
+        "image": "benux.png",
+        "title": "Benux",
+        "year": "2018",
+        "desc": "Is an android application for learning about the linux operating system (linux handbook).",
+        "link": "https://play.google.com/store/apps/details?id=com.wahyuapps.benux&hl=en&gl=US&pli=1"
+    },
+    {
+        "image": "ngimel.png",
+        "title": "Ngimel",
+        "year": "2018",
+        "desc": "is an android application to do email templates automatically in order to make it easier to write repeated emails.",
+        "link": "https://github.com/WahyuHidayattz/ngimel"
+    }
+]
+
+let skills = [
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-database-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M4 6c0 1.657 3.582 3 8 3s8 -1.343 8 -3s-3.582 -3 -8 -3s-8 1.343 -8 3"></path>
+        <path d="M4 6v6c0 1.657 3.582 3 8 3m8 -3.5v-5.5"></path>
+        <path d="M4 12v6c0 1.657 3.582 3 8 3"></path>
+        <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+        <path d="M20.2 20.2l1.8 1.8"></path>
+     </svg>`,
+        "title": "Data Analyst",
+        "desc": "Create and analyze data from scratch to then display it so that it becomes a valid report and can be evaluated."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chart-area-line" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M4 19l4 -6l4 2l4 -5l4 4l0 5l-16 0"></path>
+        <path d="M4 12l3 -4l4 2l5 -6l4 4"></path>
+     </svg>`,
+        "title": "Data Visualization",
+        "desc": "Processing raw data into visualization data so that it is easy to see trends, history, and patterns from the data."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-app-window" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+        <path d="M6 8h.01"></path>
+        <path d="M9 8h.01"></path>
+     </svg>`,
+        "title": "Web Development",
+        "desc": "Create various types of websites according to the desired function, using the PHP programming language and MySQL database."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-paint" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M5 3m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z"></path>
+        <path d="M19 6h1a2 2 0 0 1 2 2a5 5 0 0 1 -5 5l-5 0v2"></path>
+        <path d="M10 15m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
+     </svg>`,
+        "title": "Design Slicing & Coding",
+        "desc": "Perform website design slicing which is then written into code to be used on a real website."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-mobile" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z"></path>
+        <path d="M11 4h2"></path>
+        <path d="M12 17v.01"></path>
+     </svg>`,
+        "title": "Responsive Design",
+        "desc": "Convert UI design to HTML code responsive on all device sizes with the help of CSS framework (Tailwindcss)."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-ubuntu" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+        <path d="M17.723 7.41a7.992 7.992 0 0 0 -3.74 -2.162m-3.971 0a7.993 7.993 0 0 0 -3.789 2.216m-1.881 3.215a8 8 0 0 0 -.342 2.32c0 .738 .1 1.453 .287 2.132m1.96 3.428a7.993 7.993 0 0 0 3.759 2.19m4 0a7.993 7.993 0 0 0 3.747 -2.186m1.962 -3.43a8.008 8.008 0 0 0 .287 -2.131c0 -.764 -.107 -1.503 -.307 -2.203"></path>
+        <path d="M5 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+        <path d="M19 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
+     </svg>`,
+        "title": "Linux Administration",
+        "desc": "Install and configure linux servers for FTP, Web Service, Networking and other purposes."
+    },
+    {
+        "icon": `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-robot" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M7 7h10a2 2 0 0 1 2 2v1l1 1v3l-1 1v3a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-3l-1 -1v-3l1 -1v-1a2 2 0 0 1 2 -2z"></path>
+        <path d="M10 16h4"></path>
+        <circle cx="8.5" cy="11.5" r=".5" fill="currentColor"></circle>
+        <circle cx="15.5" cy="11.5" r=".5" fill="currentColor"></circle>
+        <path d="M9 7l-1 -4"></path>
+        <path d="M15 7l1 -4"></path>
+     </svg>`,
+        "title": "Telegrm Bot",
+        "desc": "Create a telegram bot using PHP and expand its functions to make work easier."
+    },
+]
+
+let tools = [
+    {
+        "icon": "excel.png",
+        "title": "Excel",
+        "link": "https://www.microsoft.com/en-us/microsoft-365/excel"
+    },
+    {
+        "icon": "powerbi.png",
+        "title": "PowerBI",
+        "link": "https://powerbi.microsoft.com/en-au/"
+    },
+    {
+        "icon": "mysql.png",
+        "title": "MySQL",
+        "link": "https://www.mysql.com/"
+    },
+    {
+        "icon": "vs_code.png",
+        "title": "VS Code",
+        "link": "https://code.visualstudio.com/"
+    },
+    {
+        "icon": "firefox.png",
+        "title": "Firefox",
+        "link": "https://www.mozilla.org/en-US/firefox/new/"
+    },
+    {
+        "icon": "xampp.png",
+        "title": "Xampp",
+        "link": "https://www.apachefriends.org/"
+    },
+    {
+        "icon": "figma.png",
+        "title": "Figma",
+        "link": "https://www.figma.com/"
+    },
+    {
+        "icon": "github.png",
+        "title": "Github",
+        "link": "https://github.com"
+    },
+    {
+        "icon": "html.png",
+        "title": "HTML",
+        "link": "https://en.wikipedia.org/wiki/HTML"
+    },
+    {
+        "icon": "tailwindcss.png",
+        "title": "Tailwindcss",
+        "link": "https://tailwindcss.com/"
+    },
+    {
+        "icon": "php.png",
+        "title": "PHP",
+        "link": "https://www.php.net/"
+    },
+    {
+        "icon": "javascript.png",
+        "title": "Javascript",
+        "link": "https://www.javascript.com/"
+    },
+]
